fccde8990f820f160f1be3440a2c8f71c8b1d006
Denis Deniben <deniben2109@gmail.com>
2020-01-11 23:16:04 +0200

    added logging example in aspect code

--- src/main/java/com/windigital/spring03/logging/LoggingAspectAround.java
+++ src/main/java/com/windigital/spring03/logging/LoggingAspectAround.java
@@ -1,4 +1,31 @@
 package com.windigital.spring03.logging;
 
-public class LogginAspectAround {
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Aspect
+@Component
+public class LoggingAspectAround {
+//    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)")
+    public void repositoryClassMethods() {};
+
+    @Around("repositoryClassMethods()")
+    public Object measureMethodExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.nanoTime();
+        Object retval = pjp.proceed();
+        long end = System.nanoTime();
+        String methodName = pjp.getSignature().getName();
+        System.out.println("Execution of " + methodName + " took " +
+                TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+        return retval;
+    }
 }
